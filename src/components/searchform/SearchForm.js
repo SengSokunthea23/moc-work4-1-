2e9a869e6cefeac5fc92d7738058b7353aaf8685
Seng Sokunthea <sengsokunthealove2345@gmail.com>
2023-08-01 17:00:03 +0700

    change button

--- src/components/searchform/SearchForm.js
+++ src/components/searchform/SearchForm.js
@@ -1,14 +1,30 @@
 import React from "react";
-function SearchForm(){
-    return(
-        <div className="container" style={{height:"170px", alignItems:"center"}}>
-            <div style={{height:"246px", marginTop:"3rem"}}>
-                <button style={{border:"1px solid black", textAlign:"center", width:"6%", height:"18%", margin:"0px 5px 0px 0px"}}>
-                    Search</button>
-                <button style={{border:"1px solid black", textAlign:"center", width:"12%", height:"18%", margin:"0px 5px 0px 0px"}}>Browse categories</button>
-                <button style={{border:"1px solid black", textAlign:"center", width:"8%", height:"18%"}}>Download</button>
-            </div>
-        </div>
-    )
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Form from 'react-bootstrap/Form';
+
+function SearchForm() {
+    return (
+        <div style={{height:"230px"}}>
+        <Tabs
+        defaultActiveKey="home"
+        transition={false}
+        className="mb-3 container mt-4"
+      >
+        <Tab eventKey="home" title="Search" className="container">
+        1. Service, product or HS code &nbsp;
+        <small>(required)</small>
+        <Form.Control type="text" placeholder="Normal text" className="row-1"/>
+      <br />
+        </Tab>
+        <Tab eventKey="profile" title="Browse categories" className="container">
+          Tab content for Profile
+        </Tab>
+        <Tab eventKey="contact" title="Download" className="container">
+          Tab content for Contact
+        </Tab>
+      </Tabs>
+      </div>
+    );
 }
 export default SearchForm;
